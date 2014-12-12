2fbb356dfb54f17523253e9df36fa09883d9c64e
Rex Posadas <rexposadas@yahoo.com>
2014-12-12 12:46:51 -0600

    added counter

---
+++ counter.go
@@ -0,0 +1,22 @@
+package simulate
+
+import (
+	"sync"
+)
+
+type Counter struct {
+	M    map[string]int
+	lock sync.Mutex
+}
+
+func (c *Counter) Add(k string, v int) {
+
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	if val, ok := c.M[k]; ok {
+		c.M[k] = val + v
+	} else {
+		c.M[k] = 1
+	}
+}
