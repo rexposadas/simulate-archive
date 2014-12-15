4dd95f4e6e2174dde7ad87dc6fd4273a951cd23b
Rex Posadas <rexposadas@yahoo.com>
2014-12-14 23:24:48 -0700

    more todo comments

--- stats.go
+++ stats.go
@@ -1,30 +1,27 @@
 package simulate
 
+// StatsObject is in charge of keep track of statistics
 type StatsObj struct {
-	Count map[string]int
-	MChan chan CountData
-}
 
-type CountData struct {
-	name  string
-	value int
+	// this variable keeps a map of strings and it's count.
+	Count map[string]int
 }
 
 // Tick adds a one (+1) to the Map for a given key
-func (s *StatsObj) Tick(t string) {
+func (s *StatsObj) Add(t string) {
 
-	// todo: implement
-	// this function adds (+1) to the map M.
+	// todo:
+	// this function adds the string t to the "Count" member variable. if
+	// t already exists in the map, then simply increate the count.
 }
 
 func (s *StatsObj) Sub(t string) {
-	//todo: implement
+
+	// todo: similar to the Add() method, but substracts a value from Count.
+
 }
 
 func (s *StatsObj) Run() {
 	s.Count = make(map[string]int)
-	s.MChan = make(chan CountData, 1000)
-
-	// todo: create a loop which consumes MChan and adds/deletes from Count
 
 }
