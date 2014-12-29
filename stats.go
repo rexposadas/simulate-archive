85301ac5e40e2f53f5b30296377b0496392b0435
Rex Posadas <rexposadas@yahoo.com>
2014-12-29 08:19:21 -0600

    move locks to common place

--- stats.go
+++ stats.go
@@ -2,7 +2,7 @@ package simulate
 
 import (
 	"sync"
-)	
+)
 
 // StatsObject is in charge of keep track of statistics
 type StatsObj struct {
@@ -11,27 +11,26 @@ type StatsObj struct {
 	Count map[string]int
 	sync.RWMutex
 }
+
 func New() *StatsObj {
 	return &StatsObj{
 		Count: make(map[string]int),
 	}
 }
+
 // Tick adds a one (+1) to the Map for a given key
 func (s *StatsObj) Add(t string) {
-	s.Lock()
-	defer s.Unlock()
 	s.SimpleMath(t, 1)
-
 }
 
 func (s *StatsObj) Sub(t string) {
-	s.Lock()
-	defer s.Unlock()
 	s.SimpleMath(t, -1)
 }
 
 // simpleMath adds/substracts an arbitrary amount from a map key
 func (s *StatsObj) SimpleMath(t string, c int) {
+	s.Lock()
+	defer s.Unlock()
 
 	if val, ok := s.Count[t]; ok {
 		s.Count[t] = val + c
